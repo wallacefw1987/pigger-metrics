0049821334594c3287fbf17341ab9a4a40b87db8
wallacefw <wallacefw@qq.com>
2018-11-02 11:12:17 +0800

    create：添加springcloud的demo中consumer端配置

--- demo-client-consumer/src/main/java/com/kvo/consumer/ConsumerApplication.java
+++ demo-client-consumer/src/main/java/com/kvo/consumer/ConsumerApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @author fanwei
- */
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerApplication {
 
     public static void main(String[] args) {
